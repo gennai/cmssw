b89ebeb935eec5ef9c66848c04eff7af9fac0920
simone gennai <simone.gennai@cern.ch>
2020-05-29 16:35:26 +0200

    addded L1TkEle filter

--- HLTrigger/HLTfilters/plugins/plugins.cc
+++ HLTrigger/HLTfilters/plugins/plugins.cc
@@ -40,6 +40,7 @@ using namespace trigger;
 #include "HLTSinglet.cc"
 
 #include "L1TkMuonFilter.h"
+#include "L1TkEleFilter.h"
 
 // filter for HLT candidates
 typedef HLTSinglet<RecoEcalCandidate> HLT1Photon;
@@ -119,6 +120,7 @@ DEFINE_FWK_MODULE(HLTBool);
 DEFINE_FWK_MODULE(HLTFiltCand);
 DEFINE_FWK_MODULE(HLTHighLevel);
 DEFINE_FWK_MODULE(L1TkMuonFilter);
+DEFINE_FWK_MODULE(L1TkEleFilter);
 DEFINE_FWK_MODULE(HLT2CaloJetCaloJet);
 DEFINE_FWK_MODULE(HLT2CaloJetCaloMET);
 DEFINE_FWK_MODULE(HLT2CaloJetMET);
